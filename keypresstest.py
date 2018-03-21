ebccf65b7341974511785058f8657b805ed203d5
Jonathan Poelman <jonathan.poelman@ugent.be>
2018-03-21 21:36:03 +0100

    print vervangen door stdout write

--- keypresstest.py
+++ keypresstest.py
@@ -1,4 +1,5 @@
 import pygame
+import sys
 
 teamOccupying = 0
 pygame.init()
@@ -10,14 +11,10 @@ while True:
     for event in events:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_KP1:
-                teamOccupying = 1
+                sys.stdout.write(1)
                 screen.fill([255, 0, 0])
                 pygame.display.update()
             if event.key == pygame.K_KP2:
-                teamOccupying = 2  
+                sys.stdout.write(2)
                 screen.fill([0, 255, 0])
-                pygame.display.update()     
-            if event.key == pygame.K_KP3:
-                teamOccupying = 2  
-                screen.fill([0, 255, 0])
-                pygame.display.update()      
+                pygame.display.update()                  
